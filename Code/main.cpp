ca8747eec09117171a24cd566e1ef22a7ec96f24
abdullah-netiz <i243164@isb.nu.edu.pk>
2025-04-30 03:20:11 +0500

    Set up the OOP Project

---
+++ Code/main.cpp
@@ -0,0 +1,17 @@
+//Abdullah
+//24I-3164
+//SE-C
+#include<iostream>
+#include "player.h"
+#include "shop.h"
+#include"training.h"
+#include "battle.h"
+
+using namespace std;
+
+int main()
+{
+
+
+return 0;
+}
